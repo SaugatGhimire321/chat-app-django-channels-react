ea03e586fd90afea08e2cc86b1202ed0dfb29c88
Saugat Ghimire <saugatghimire321@gmail.com>
2024-01-06 23:43:26 +0545

    chore: basic chat app with echo back to every instance

--- apps/chats/consumers.py
+++ apps/chats/consumers.py
@@ -1,5 +1,5 @@
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import JsonWebsocketConsumer
- 
  
 class ChatConsumer(JsonWebsocketConsumer):
     """
@@ -15,6 +15,10 @@ class ChatConsumer(JsonWebsocketConsumer):
         print("Connected!")
         self.room_name = "home"
         self.accept()
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name,
+            self.channel_name,
+        )
         self.send_json(
             {
                 "type": "welcome_message",
@@ -27,5 +31,18 @@ class ChatConsumer(JsonWebsocketConsumer):
         return super().disconnect(code)
  
     def receive_json(self, content, **kwargs):
-        print(content)
+        message_type = content["type"]
+        if message_type == "chat_message":
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_name,
+                {
+                    "type": "chat_message_echo",
+                    "name": content["name"],
+                    "message": content["message"],
+                },
+            )
         return super().receive_json(content, **kwargs)
+    
+    def chat_message_echo(self, event):
+        print(event)
+        self.send_json(event)
