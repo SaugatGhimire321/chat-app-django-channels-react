518c6ff3ea235cbe8e4f056517dbe52fca05a8e0
Saugat Ghimire <saugatghimire321@gmail.com>
2024-01-07 13:38:13 +0545

    fix: wrap protected route to chats route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Chat } from "./components/Chat";
 import { Login } from "./components/Login";
 import { Navbar } from "./components/Navbar";
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 export default function App() {
   return (
@@ -17,7 +18,14 @@ export default function App() {
             </AuthContextProvider>
           }
         >
-          <Route path="" element={<Chat />} />
+          <Route
+            path=""
+            element={
+              <ProtectedRoute>
+                <Chat />
+              </ProtectedRoute>
+            }
+          />
           <Route path="login" element={<Login />} />
         </Route>
       </Routes>
